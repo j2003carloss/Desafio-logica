db78329c1b9aae2f210abc6c21515b7cb3940eac
j2003carloss <lcp3106@gmail.com>
2024-01-24 22:40:00 -0300

    Atualização

--- main.c
+++ main.c
@@ -1,24 +1,30 @@
 #include <stdio.h>
- 
-    int main(){
-        int nivelDoJogador=20000;
-        char nome[30];
+int main(){
+float nivelDoPersonagem;
+char nomeDoPersonagem[30];
 
-            printf("Digite o nome do seu personagem:");
-            scanf(" %c", &nome);
-            printf("Digite o nivel do seu personagem:");
-            scanf("%d", &nivelDoJogador);
+    printf("Digite o nome do seu Personagem:");
+    scanf("%s",&nomeDoPersonagem);
+    printf("Digite o nivel de XP do Personagem:\n");
+    scanf("%f",&nivelDoPersonagem);
 
-              switch (nivelDoJogador < 1000) {
-        case 1:
-            printf("Nível do jogador acima de 1000.\n");
-            break;
-        default:
-            printf("Nível do jogador 1000 ou abaixo.\n");
+    if(nivelDoPersonagem < 1000){
+            printf("%s o nivel de xp é Ferro",nomeDoPersonagem);
+    }else if(nivelDoPersonagem >= 1001 && nivelDoPersonagem <= 2000){
+            printf("%s o nivel de xp é Bronze",nomeDoPersonagem);
+    }else if (nivelDoPersonagem >= 2001 && nivelDoPersonagem <= 5000){
+            printf("%s o nivel de xp é Prata",nomeDoPersonagem);
+    }else if (nivelDoPersonagem >= 5001 && nivelDoPersonagem <= 7000){
+            printf("%s o nivel de xp é Ouro", nomeDoPersonagem);
+    }else if (nivelDoPersonagem >= 7001 && nivelDoPersonagem <= 8000){
+            printf("%s o nivel de xp é Platina",nomeDoPersonagem);
+    }else if (nivelDoPersonagem >= 8001 && nivelDoPersonagem <= 9001){
+            printf("%s o nivel de xp é Ascendente");
+    }else if (nivelDoPersonagem >= 9001 && nivelDoPersonagem <= 10000){
+            printf ("%s o nivel de xp é Imortal",nomeDoPersonagem);
+    }else if (nivelDoPersonagem > 10001){
+            printf("%s o nivel de xp é Radiante",nomeDoPersonagem);
     }
 
-
-            return 0;
-
-    
-    }
+        return 0;
+}
